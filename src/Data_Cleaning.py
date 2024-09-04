9536b0ef2bffdefc110196b2067d4f941d11a388
Harjit Gakhal <harjitgakhal@Harjits-MacBook-Pro.local>
2024-09-04 17:46:54 +0100

    Adding error handling

--- src/Data_Cleaning.py
+++ src/Data_Cleaning.py
@@ -6,16 +6,37 @@ class Cleaner:
     def __init__(self, ModelBuilder=None, pre = None, re = None, acc = None):
 
         self.ModelBuilder = ModelBuilder
-        self.pre = Precision()
-        self.re = Recall()
-        self.acc = BinaryAccuracy()
+        self.pre = Precision() if pre is None else pre
+        self.re = Recall() if re is None else re
+        self.acc = BinaryAccuracy() if acc is None else acc
 
     def Testing_data_cleaner(self, Test_data):
+        try:
+            if Test_data is None:
+                raise ValueError("Testing data cannot be empty")
+            if not hasattr(Test_data, 'as_numpy_iterator'):
+                raise ValueError("Testing data must be Tensorflow object")
+        except (ValueError) as e:
+           print(f'Error: {e}')
+           raise
+
         labels_testing = []
         X_test = []
 
         for batch in Test_data.as_numpy_iterator():
             x_test, y_test = batch
+            # Error checking
+            print(x_test.shape)
+            print(y_test.shape)
+            print('------------')
+
+            print(x_test[0])
+            print(y_test[0])
+
+
+
+
+
             y_test = [
                 np.where(row == 1)[0][0].tolist() for row in y_test
             ]  # Gets the position of the 1 to determine the type of tumor and assigns to a list
--- src/Data_Loader.py
+++ src/Data_Loader.py
@@ -5,12 +5,36 @@ class DataLoader:
                  batch_size = 32,
                  validation_split = 0.2,
                  seed =42):
+        try:
+            if not (isinstance(image_size,tuple) and len(image_size) == 2 and all(isinstance(i,int) for i in image_size)):
+                raise ValueError("Image size must be a tuple of two integers")
+            if not isinstance(batch_size, int) or batch_size <= 0:
+                raise ValueError("Batch size must be positive integer")
+            if not (0 < validation_split <1):
+                raise ValueError("Validation split must be between 0 and 1")
+            if not isinstance(seed, int):
+                raise ValueError("Seed must be an integer")
+            
+        except (TypeError, ValueError) as e:
+            print(f"Error: {e}")
+            raise
+
         self.image_size = image_size
         self.batch_size = batch_size
         self.validation_split = validation_split
         self.seed = seed
 
     def Load_Training_data(self, training_path):
+
+        try:
+            if not isinstance(training_path, str):
+                raise ValueError("Training path should be a string")
+        except (ValueError) as e:
+            print(f"Error: {e}")
+            raise
+
+        # Proceed with data loading
+
         Training_data = tf.keras.utils.image_dataset_from_directory(
         training_path,
         labels='inferred',
@@ -35,6 +59,16 @@ class DataLoader:
         return Validation_data
 
     def Load_Testing_data(self,testing_path):
+
+        try:
+            if not isinstance(testing_path, str):
+                raise ValueError("Testing path should be a string")
+        except (ValueError) as e:
+            print(f"Error: {e}")
+            raise
+
+        # Proceed to load testing data
+
         Testing_data = tf.keras.utils.image_dataset_from_directory(
         testing_path,
         labels='inferred',
@@ -42,3 +76,16 @@ class DataLoader:
         batch_size=self.batch_size,
     )
         return Testing_data
+    
+    def validate_data(self, dataset):
+        try:
+            for images,labels in dataset.as_numpy_iterator(): #taking first batch
+                print('images', images[1])
+                print('labels', labels[1])
+                if images.shape[1:] != (256,256,3):
+                    raise ValueError ('Image should be of size (256,256,3)')
+                if labels.shape[1] != 1:
+                    raise ValueError ('Labels should be of length 1' )
+        except ValueError as e:
+            print(f'Data validation error {e}')
+            raise
