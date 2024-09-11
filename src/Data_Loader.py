c9586feb7477703a21ac7f340a5a8a203aa644a3
Harjit Gakhal <harjitgakhal@Harjits-MacBook-Pro.local>
2024-09-11 19:16:18 +0100

    Updating error handling

--- src/Data_Loader.py
+++ src/Data_Loader.py
@@ -9,7 +9,7 @@ class DataLoader:
         try:
             if not (isinstance(image_size,tuple) and len(image_size) == 2 and all(isinstance(i,int) for i in image_size)):
                 raise ValueError("Image size must be a tuple of two integers")
-            if not isinstance(batch_size, int) or batch_size <= 0:
+            if (not isinstance(batch_size, int) or batch_size <= 0):
                 raise ValueError("Batch size must be positive integer")
             if not (0 < validation_split <1):
                 raise ValueError("Validation split must be between 0 and 1")
@@ -41,6 +41,7 @@ class DataLoader:
         labels='inferred',
         image_size=self.image_size, # We are adjusting the images so they become 256x256 images instead.
         batch_size=self.batch_size,
+        color_mode = 'grayscale', # images are all in black and white, so 1D
         validation_split=self.validation_split, # We will utilise 20% of the data from the training data for validation.
         subset='training',
         seed=42
@@ -53,6 +54,7 @@ class DataLoader:
         labels='inferred',
         image_size=self.image_size, # We are adjusting the images so they become 256x256 images instead.
         batch_size=self.batch_size,
+        color_mode = 'grayscale',
         validation_split=self.validation_split, # We will utilise 20% of the data from the training data for validation.
         subset='validation',
         seed=42
@@ -74,6 +76,7 @@ class DataLoader:
         labels='inferred',
         image_size=self.image_size,
         batch_size=self.batch_size,
+        color_mode = 'grayscale',
     )
         return Testing_data
     
@@ -81,16 +84,34 @@ class DataLoader:
         try:
             for images,labels in dataset.as_numpy_iterator(): #taking first batch
                 # Checking shape size
-                if images.shape[1:] != (256,256,3):
-                    raise ValueError ('Image should be of size (256,256,3)')
+                if images.shape[1:] != (256,256,1):
+                    raise ValueError ('Image should be of size (256,256,1)')
                 
                 # Checking the labels are integers
                 if not np.issubdtype(labels.dtype, np.integer):
                     raise ValueError(f"labels should be integers, but got {labels.dtype}") 
 
-                # Checking the dimension of the labels 
-                if labels.ndim != 1:
-                    raise ValueError (f'Labels should be a 1D array of shape (batch_size,), instead got {labels.shape}')
+                # Checking the dimension of the labels
+            if labels.ndim != 1:
+                raise ValueError(f'Labels should be a 1D array of shape (batch_size,), instead got {labels.shape}')
         except ValueError as e:
-            print(f'Data validation error {e}')
+            print(f'Data validation error: {e}')
+            raise
+
+    def validate_data_float(self, dataset):
+        try:
+            for images,labels in dataset.as_numpy_iterator(): #taking first batch
+                # Checking shape size
+                if images.shape[1:] != (256,256,1):
+                    raise ValueError ('Image should be of size (256,256,1)')
+                
+                # Checking the labels are integers
+                if not np.issubdtype(labels.dtype, np.float32):
+                    raise ValueError(f"labels should be floating numbers, but got {labels.dtype}") 
+
+                # Checking the dimension of the labels
+                if labels.ndim != 2:
+                    raise ValueError(f'Labels should be a 1D array of shape ({self.batch_size},4), instead got {labels.shape}')
+        except ValueError as e:
+            print(f'Data validation error: {e}')
             raise
